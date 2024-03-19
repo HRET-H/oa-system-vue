da9a8364089dc04d2905fd68e6c038d759d05081
HRET <2126366332@qq.com>
2024-03-19 19:18:55 +0800

    导航栏更新

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
 import pinia from './stores/index'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
 app.use(router)
 app.use(pinia)
 app.mount('#app')
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,6 +11,9 @@ import MenuTree from '@/components/menu/MenuTree.vue'
 axios.defaults.baseURL = 'http://localhost:9999'
 
 // 定义一个ref变量，用来存储数据
+let NavDrawer = ref('flase')
+
+// 定义一个ref变量，用来存储数据
 let MenuData = ref([])
 
 // 使用onMounted钩子函数，获取菜单数据
@@ -23,11 +26,32 @@ onMounted(() => {
 
 <template>
   <div id="oa-home">
-    <el-header>
-      <el-col> </el-col>
-      <el-col>
-        <MenuTree :data="MenuData" mode="horizontal" />
-      </el-col>
+    <el-header style="background-color: rgb(35, 43, 64); height: 30%">
+      <span>
+        <el-image
+          style="width: 30px; height: 40px; position: relative; top: 15px"
+          src="https://hret0721.oss-cn-beijing.aliyuncs.com/oa-system/oa-Logo.png"
+          fit="fill"
+        />
+        &nbsp;
+        <h4 style="width: 15%; color: #fff; display: inline-block">
+          OA数字化办公系统
+        </h4>
+      </span>
+      <MenuTree
+        :data="MenuData"
+        mode="horizontal"
+        style="width: 60%; display: inline-block"
+      />
+      <el-icon
+        color="#fff"
+        @click="NavDrawer = true"
+        style="position: relative; right: 33%"
+        ><Fold
+      /></el-icon>
+      <el-drawer v-model="NavDrawer" :with-header="false">
+        <span>Hi there!</span>
+      </el-drawer>
     </el-header>
     <router-view></router-view>
   </div>
