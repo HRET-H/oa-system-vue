47d089379e17e5822107ae50971fbd56f18ec87c
root <2450094803@qq.com>
2024-03-27 11:39:33 +0800

    职位管理候选人

--- src/views/recruit/addCanDidate.vue
+++ src/views/recruit/addCanDidate.vue
@@ -1,5 +1,34 @@
 <template>
-  <div>1</div>
+  <div>
+    <h1>
+      <div>
+        <span style="color: 24, 144, 255; font-size: 36px"><b>|</b></span>
+        <span style="font-size: 26px">职位信息</span>
+      </div>
+    </h1>
+    <br />
+    <span>
+      &nbsp;&nbsp;&nbsp;&nbsp; 招聘职位:&nbsp;
+      <el-select placeholder="全部" style="width: 350px">
+        <el-option> </el-option>
+      </el-select>
+    </span>
+    <span>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      用人部门:&nbsp;
+      <el-select placeholder="全部" style="width: 350px">
+        <el-option> </el-option>
+      </el-select>
+    </span>
+    <br />
+    <br />
+    <span>
+      &nbsp;&nbsp;&nbsp;&nbsp; 添加流程:&nbsp;
+      <el-select placeholder="全部" style="width: 350px">
+        <el-option> </el-option>
+      </el-select>
+    </span>
+  </div>
 </template>
 
 <script>
