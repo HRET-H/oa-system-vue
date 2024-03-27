10cd202a9594ee93737220d4c12187ecfba67f34
HRET <2126366332@qq.com>
2024-03-27 09:15:31 +0800

    部门员工信息选择通用组件 依赖更改建议重新安装依赖

--- src/main.js
+++ src/main.js
@@ -7,6 +7,18 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 // 引入全局组件
 import ui from '@/utils/global.js'
 
+// Vuetify
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives
+})
+
 const app = createApp(App)
 // 注册路由
 app.use(router)
@@ -14,6 +26,8 @@ app.use(router)
 app.use(pinia)
 // 注册全局组件
 app.use(ui)
+// Vuetify
+app.use(vuetify)
 // 为 Element Plus 图标库注册全局组件
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
